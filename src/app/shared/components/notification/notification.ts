64ad3074a455abc25cebae7d727bec0031cb1ded
gabriiel23 <gabriel.diaz.23.02.05@gmail.com>
2025-08-10 02:40:45 -0500

    Notificationes

--- src/app/shared/components/notification/notification.ts
+++ src/app/shared/components/notification/notification.ts
@@ -1,22 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-notification',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './notification.html',
-  styleUrls: ['./notification.scss'],
-  animations: [
-    trigger('fadeScale', [
-      state('hidden', style({ opacity: 0, transform: 'scale(0.95)' })),
-      state('visible', style({ opacity: 1, transform: 'scale(1)' })),
-      transition('hidden <=> visible', [
-        animate('200ms ease-in-out')
-      ])
-    ])
-  ]
+  styleUrls: ['./notification.scss']
 })
 export class NotificationComponent {
   @Input() message: string = '';
